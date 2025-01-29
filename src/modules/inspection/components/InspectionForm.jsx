092b7952d6dcb4d3f01c36d6a99b95287ad8af8d
ARSBry <brayhan.vargas@ars-combinatoria.com>
2025-01-29 00:41:08 -0500

    some pending fixes

--- src/modules/inspection/components/InspectionForm.jsx
+++ src/modules/inspection/components/InspectionForm.jsx
@@ -50,17 +50,161 @@ export const InspectionForm = ({
 		}
 	}, [ratesByRisk, totalInsuredValue, setValue]);
 
-	// Helper function to handle formatting on blur
-	const handleCurrencyBlur = (fieldName) => (event) => {
-		const rawValue = event.target.value.replace(/[^0-9.-]+/g, ""); // Remove non-numeric characters
-		const numericValue = Number.parseFloat(rawValue);
-		console.log(numericValue);
-	};
-
-	console.log(watch("totalInsuredValue"));
-
 	return (
 		<form id="inspection-form" onSubmit={onSubmit}>
+			{/* Compañía Asesora (JAC, IAR)*/}
+			<FormControl
+				fullWidth
+				margin="dense"
+				error={!!errors.insuredId}
+				sx={{ mb: 2, mt: 1 }}
+			>
+				<InputLabel id="simple-select-label-insured">
+					Compañía Asesora
+				</InputLabel>
+				<Controller
+					name="advisorCompany"
+					labelId="simple-select-label-insured"
+					control={control}
+					render={({ field }) => (
+						<Select
+							{...field}
+							label="Compañía Asesora"
+							error={!!errors.advisorCompany}
+						>
+							<MenuItem key={1} value={"JAC"}>
+								JAC
+							</MenuItem>
+							<MenuItem key={2} value={"IAR"}>
+								IAR
+							</MenuItem>
+						</Select>
+					)}
+				/>
+				{errors.advisorCompany && (
+					<Typography color="error.main" fontSize="small">
+						{errors.advisorCompany.message}
+					</Typography>
+				)}
+			</FormControl>
+
+			{/* Servicio Asignado (Select con INSPECCION, REINSPECCION, CAPACITACION, SEGUIMIENTO A RECOMENDACIONES, IRV - INSPECCION REMOTA POR VIDEO) */}
+			<FormControl
+				fullWidth
+				margin="dense"
+				error={!!errors.serviceAssigned}
+				sx={{ mb: 2 }}
+			>
+				<InputLabel id="simple-select-label-service">
+					Servicio Asignado
+				</InputLabel>
+				<Controller
+					name="serviceAssigned"
+					labelId="simple-select-label-service"
+					control={control}
+					render={({ field }) => (
+						<Select
+							{...field}
+							label="Servicio Asignado"
+							error={!!errors.serviceAssigned}
+						>
+							<MenuItem key={1} value={"INSPECCION"}>
+								INSPECCION
+							</MenuItem>
+							<MenuItem key={2} value={"REINSPECCION"}>
+								REINSPECCION
+							</MenuItem>
+							<MenuItem key={3} value={"CAPACITACION"}>
+								CAPACITACION
+							</MenuItem>
+							<MenuItem key={4} value={"SEGUIMIENTO A RECOMENDACIONES"}>
+								SEGUIMIENTO A RECOMENDACIONES
+							</MenuItem>
+							<MenuItem key={5} value={"IRV - INSPECCION REMOTA POR VIDEO"}>
+								IRV - INSPECCION REMOTA POR VIDEO
+							</MenuItem>
+						</Select>
+					)}
+				/>
+			</FormControl>
+
+			{/* Número de Póliza */}
+			<TextField
+				fullWidth
+				label="Número de Póliza"
+				variant="outlined"
+				type="number"
+				sx={{ mb: 2 }}
+				{...register("policyNumber")}
+				error={!!errors.policyNumber}
+				helperText={errors.policyNumber?.message}
+			/>
+
+			{/* Tipo de Trabajo (Select con INSPECCION ESTANDAR, RUTA 21} */}
+			<FormControl
+				fullWidth
+				margin="dense"
+				error={!!errors.jobType}
+				sx={{ mb: 2 }}
+			>
+				<InputLabel id="simple-select-label-job">Tipo de Trabajo</InputLabel>
+				<Controller
+					name="jobType"
+					labelId="simple-select-label-job"
+					control={control}
+					render={({ field }) => (
+						<Select {...field} label="Tipo de Trabajo" error={!!errors.jobType}>
+							<MenuItem key={1} value={"INSPECCION ESTANDAR"}>
+								INSPECCION ESTANDAR
+							</MenuItem>
+							<MenuItem key={2} value={"RUTA 21"}>
+								RUTA 21
+							</MenuItem>
+						</Select>
+					)}
+				/>
+			</FormControl>
+
+			{/* Código de Intermediario */}
+			<TextField
+				fullWidth
+				label="Código de Intermediario"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("intermediaryCode")}
+				error={!!errors.intermediaryCode}
+				helperText={errors.intermediaryCode?.message}
+			/>
+
+			{/* Inspector */}
+			<FormControl
+				fullWidth
+				margin="dense"
+				error={!!errors.employeeId}
+				sx={{ mb: 2, mt: 0 }}
+			>
+				<InputLabel id="simple-select-label-employee">Inspector</InputLabel>
+				<Controller
+					name="employeeId"
+					labelId="simple-select-label-employee"
+					control={control}
+					render={({ field }) => (
+						<Select {...field} label="Inspector" error={!!errors.employeeId}>
+							{employeesSelect?.map((employee) => (
+								<MenuItem key={employee.value} value={employee.value}>
+									{employee.label}
+								</MenuItem>
+							))}
+						</Select>
+					)}
+				/>
+				{errors.employeeId && (
+					<Typography color="error.main" fontSize="small">
+						{errors.employeeId.message}
+					</Typography>
+				)}
+			</FormControl>
+
 			{/* Asegurado */}
 			<FormControl
 				fullWidth
@@ -89,7 +233,8 @@ export const InspectionForm = ({
 					</Typography>
 				)}
 			</FormControl>
-			{/* Sede Solicitud Asegurado */}
+
+			{/* Sede Asegurado */}
 			<FormControl
 				fullWidth
 				margin="dense"
@@ -123,6 +268,7 @@ export const InspectionForm = ({
 					</Typography>
 				)}
 			</FormControl>
+
 			{/* Compañía Aseguradora */}
 			<FormControl
 				fullWidth
@@ -157,6 +303,7 @@ export const InspectionForm = ({
 					</Typography>
 				)}
 			</FormControl>
+
 			{/* Sucursal */}
 			<FormControl
 				fullWidth
@@ -185,34 +332,19 @@ export const InspectionForm = ({
 					</Typography>
 				)}
 			</FormControl>
-			{/* Inspector */}
-			<FormControl
+
+			{/* Centro de Costo */}
+			<TextField
 				fullWidth
-				margin="dense"
-				error={!!errors.employeeId}
-				sx={{ mb: 2, mt: 0 }}
-			>
-				<InputLabel id="simple-select-label-employee">Inspector</InputLabel>
-				<Controller
-					name="employeeId"
-					labelId="simple-select-label-employee"
-					control={control}
-					render={({ field }) => (
-						<Select {...field} label="Inspector" error={!!errors.employeeId}>
-							{employeesSelect?.map((employee) => (
-								<MenuItem key={employee.value} value={employee.value}>
-									{employee.label}
-								</MenuItem>
-							))}
-						</Select>
-					)}
-				/>
-				{errors.employeeId && (
-					<Typography color="error.main" fontSize="small">
-						{errors.employeeId.message}
-					</Typography>
-				)}
-			</FormControl>
+				label="Centro de Costo/Código de Sucursal"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("costCenter")}
+				error={!!errors.costCenter}
+				helperText={errors.costCenter?.message}
+			/>
+
+			{/* Campos de fechas */}
 			<TextField
 				fullWidth
 				label="Fecha de Solicitud"
@@ -254,17 +386,168 @@ export const InspectionForm = ({
 			/>
 			<TextField
 				fullWidth
-				label="Fecha de Entrega"
+				label="Fecha de Entrega del Inspector"
 				variant="outlined"
 				type="date"
 				sx={{ mb: 2 }}
-				{...register("deliveryDate")}
-				error={!!errors.deliveryDate}
-				helperText={errors.deliveryDate?.message}
+				{...register("deliveryDateInspector")}
+				error={!!errors.deliveryDateInspector}
+				helperText={errors.deliveryDateInspector?.message}
 				slotProps={{
 					inputLabel: { shrink: true },
 				}}
 			/>
+
+			<TextField
+				fullWidth
+				label="Fecha de Entrega a la Aseguradora"
+				variant="outlined"
+				type="date"
+				sx={{ mb: 2 }}
+				{...register("deliveryDateToInsurer")}
+				error={!!errors.deliveryDateToInsurer}
+				helperText={errors.deliveryDateToInsurer?.message}
+				slotProps={{
+					inputLabel: { shrink: true },
+				}}
+			/>
+
+			{/* Código CIIU */}
+			<TextField
+				fullWidth
+				label="Código CIIU"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("ciiuCode")}
+				error={!!errors.ciiuCode}
+				helperText={errors.ciiuCode?.message}
+			/>
+
+			{/* Descripción CIIU */}
+			<TextField
+				fullWidth
+				label="Descripción CIIU"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("ciiuDescription")}
+				error={!!errors.ciiuDescription}
+				helperText={errors.ciiuDescription?.message}
+			/>
+
+			{/* Coordenadas */}
+			<TextField
+				fullWidth
+				label="Coordenadas (Longitud, Latitud)"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("coordinates")}
+				error={!!errors.coordinates}
+				helperText={errors.coordinates?.message}
+			/>
+
+			{/* Tipo de Construcción */}
+
+			{/* Año de Construcción */}
+			<TextField
+				fullWidth
+				label="Año de Construcción"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("constructionYear")}
+				error={!!errors.constructionYear}
+				helperText={errors.constructionYear?.message}
+			/>
+
+			{/* Número de Pisos */}
+			<TextField
+				fullWidth
+				label="Número de Pisos"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("floorsNumber")}
+				error={!!errors.floorsNumber}
+				helperText={errors.floorsNumber?.message}
+			/>
+
+			{/* Técnico ANS */}
+			<TextField
+				fullWidth
+				label="Técnico ANS"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("tecnincANS")}
+				error={!!errors.ansTechnician}
+				helperText={errors.ansTechnician?.message}
+			/>
+
+			{/* Retraso ANS */}
+			<TextField
+				fullWidth
+				label="Retraso ANS"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("delayANS")}
+				error={!!errors.ansDelay}
+				helperText={errors.delayANS?.message}
+			/>
+
+			{/* Gastos - Cobro de viaticos a la aseguradora*/}
+			<TextField
+				fullWidth
+				label="Gastos"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("expenses")}
+				error={!!errors.expenses}
+				helperText={errors.expenses?.message}
+			/>
+
+			{/* Viaticos - Pago de viaticos al inspector */}
+			<TextField
+				fullWidth
+				label="Viáticos"
+				variant="outlined"
+				sx={{ mb: 2 }}
+				{...register("travelExpenses")}
+				error={!!errors.travelExpenses}
+				helperText={errors.travelExpenses?.message}
+			/>
+
+			{/* Estado de viaticos - (No hay viaticos, pagados, por pagar)*/}
+			<FormControl
+				fullWidth
+				margin="dense"
+				error={!!errors.travelExpensesStatus}
+				sx={{ mb: 2 }}
+			>
+				<InputLabel id="simple-select-label-travel-expenses-status">
+					Estado de Viáticos
+				</InputLabel>
+				<Controller
+					name="travelExpensesStatus"
+					labelId="simple-select-label-travel-expenses-status"
+					control={control}
+					render={({ field }) => (
+						<Select
+							{...field}
+							label="Estado de Viáticos"
+							error={!!errors.travelExpensesStatus}
+						>
+							<MenuItem key={1} value={"NO HAY VIATICOS"}>
+								NO HAY VIATICOS
+							</MenuItem>
+							<MenuItem key={2} value={"PAGADOS"}>
+								PAGADOS
+							</MenuItem>
+							<MenuItem key={3} value={"POR PAGAR"}>
+								POR PAGAR
+							</MenuItem>
+						</Select>
+					)}
+				/>
+			</FormControl>
+
+			{/* Tipo de Riesgo */}
 			<FormControl
 				fullWidth
 				margin="dense"
@@ -298,6 +581,8 @@ export const InspectionForm = ({
 					</Typography>
 				)}
 			</FormControl>
+
+			{/* Valor Asegurado */}
 			<Controller
 				name="totalInsuredValue"
 				control={control}
@@ -318,12 +603,10 @@ export const InspectionForm = ({
 						sx={{ mb: 2 }}
 						error={!!errors.totalInsuredValue}
 						helperText={errors.totalInsuredValue?.message}
-						slotProps={{
-							inputLabel: { shrink: true },
-						}}
 					/>
 				)}
 			/>
+			{/* Tarifa */}
 			<TextField
 				fullWidth
 				label="Tarifa"
@@ -336,54 +619,8 @@ export const InspectionForm = ({
 						: "Sin tarifa aplicable"
 				}
 			/>
-			<TextField
-				fullWidth
-				label="Pago Extra por Transporte"
-				variant="outlined"
-				sx={{ mb: 2 }}
-				{...register("extraPaymentForTransport")}
-				error={!!errors.extraPaymentForTransport}
-				helperText={errors.extraPaymentForTransport?.message}
-				slotProps={{
-					inputLabel: { shrink: true },
-				}}
-			/>
-			<TextField
-				fullWidth
-				label="Pago Extra por Movilización"
-				variant="outlined"
-				sx={{ mb: 2 }}
-				{...register("extraPaymentForMobilization")}
-				error={!!errors.extraPaymentForMobilization}
-				helperText={errors.extraPaymentForMobilization?.message}
-				slotProps={{
-					inputLabel: { shrink: true },
-				}}
-			/>
-			<TextField
-				fullWidth
-				label="Pago Extra por Manutención"
-				variant="outlined"
-				sx={{ mb: 2 }}
-				{...register("extraPaymentForMaintenance")}
-				error={!!errors.extraPaymentForMaintenance}
-				helperText={errors.extraPaymentForMaintenance?.message}
-				slotProps={{
-					inputLabel: { shrink: true },
-				}}
-			/>
-			<TextField
-				fullWidth
-				label="Pago Extra por Alojamiento"
-				variant="outlined"
-				sx={{ mb: 2 }}
-				{...register("extraPaymentForAccommodation")}
-				error={!!errors.extraPaymentForAccommodation}
-				helperText={errors.extraPaymentForAccommodation?.message}
-				slotProps={{
-					inputLabel: { shrink: true },
-				}}
-			/>
+
+			{/* Pago Diferenciado */}
 			<TextField
 				fullWidth
 				label="Pago Diferenciado"
@@ -392,10 +629,9 @@ export const InspectionForm = ({
 				{...register("differentiatedPayment")}
 				error={!!errors.differentiatedPayment}
 				helperText={errors.differentiatedPayment?.message}
-				slotProps={{
-					inputLabel: { shrink: true },
-				}}
 			/>
+
+			{/* Comentarios */}
 			<TextField
 				fullWidth
 				label="Comentarios"
@@ -406,9 +642,6 @@ export const InspectionForm = ({
 				{...register("comments")}
 				error={!!errors.comments}
 				helperText={errors.comments?.message}
-				slotProps={{
-					inputLabel: { shrink: true },
-				}}
 			/>
 		</form>
 	);
